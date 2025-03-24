0cc82239e403dfeb84301518ff4636ac4f0d5ef1
YoungTony1695 <baothanhnguyen1695@gmail.com>
2025-03-25 01:35:17 +0700

    khoitao

---
+++ admin/inc/footer.php
@@ -0,0 +1,12 @@
+ <div class="clear">
+        </div>
+    </div>
+    <div class="clear">
+    </div>
+    <div id="site_info">
+        <p>
+         &copy; Copyright <a href="http://trainingwithliveproject.com">@2025</a>.Đại Học Phú Xuân_Công Nghệ Thông Tin.
+        </p>
+    </div>
+</body>
+</html>
---
+++ admin/index.php
@@ -0,0 +1,14 @@
+﻿<?php include 'inc/header.php';?>
+<?php include 'inc/sidebar.php';?>
+
+        <div class="grid_8">
+            <div class="box round first grid">
+                <h2> TRANG ADMIN</h2>
+                <div class="block">               
+                  XIN CHÀO ADMIN  
+                  <img src="img/anhnen.jpg" width="100%" >  
+
+                </div>
+            </div>
+        </div>
+<?php include 'inc/footer.php';?>
